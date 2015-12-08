f8873b12822ed52c4d9d52cf9c9f0364dc7ba244
phdomingo19 <patrickleinieldomingo@yahoo.com>
2015-12-08 15:10:36 +0800

    Updated App Files

---
+++ app/src/main/java/com/example/patrick/radoctor/DrugsSpecific.java
@@ -0,0 +1,7 @@
+package com.example.patrick.radoctor;
+
+/**
+ * Created by Patrick on 12/3/2015.
+ */
+public class DrugsSpecific {
+}
---
+++ app/src/main/java/com/example/patrick/radoctor/InputFilterMinMax.java
@@ -0,0 +1,7 @@
+package com.example.patrick.radoctor;
+
+/**
+ * Created by Patrick on 12/1/2015.
+ */
+public class InputFilterMinMax {
+}
